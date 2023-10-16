8d943b3d491e2ab328e7ad7de8f4d4868742dcf9
Loris <loris.martini@buonarroti.tn.it>
2023-10-16 15:32:41 +0200

    feat: classe Conttrollo che fa controlli di vario genere

--- src/classe/TestClasse.java
+++ src/classe/TestClasse.java
@@ -1,21 +1,5 @@
 package classe;
 
-import java.util.Scanner;
 public class TestClasse {
-    public static void main(String[] args) {
-        Studente s1 = null;
 
-        //Scanner input = new Scanner(System.in);
-
-        try{
-            s1 = new Studente("GiNo", "mArTiNI rOSsI");
-            System.out.println(s1.toString());
-        }catch(ArithmeticException e){
-            System.out.println("Errore 1: " + e.getMessage());
-        }catch(NullPointerException e) {
-            System.out.println("Errore 2: " + e.getMessage());
-        }catch(Exception e){
-            System.out.println("Errore 3: " + e.getMessage());
-        }
-    }
 }
