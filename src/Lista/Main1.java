62bf61de18d428bf917383928edc7772db356140
loris.martini <loris.martini@buonarroti.tn.it>
2024-03-12 10:15:35 +0100

    feat: Esercizio Automobile Arryalist (vedi classroom)

--- src/Lista/Main1.java
+++ src/Lista/Main1.java
@@ -4,20 +4,23 @@ public class Main1 {
     public static void main(String[] args) {
         // Esempio di utilizzo delle classi
         try {
-            MezzoDiTrasporto m2 = new MezzoDiTrasporto(500, "FIAT");
-            System.out.println(m2.info());
+            MezzoDiTrasporto m = new MezzoDiTrasporto(500, "FIAT");
+            System.out.println(m.info());
 
-            Automobile a2 = new Automobile(800, "LAND ROVER");
+            Automobile a = new Automobile(800, "LAND ROVER");
             Persona persona1 = new Persona("Persona1");
             Persona persona2 = new Persona("Persona2");
 
-            a2.aggiungiPasseggero(persona1);
-            a2.aggiungiPasseggero(persona2);
+            a.aggiungiPasseggero(persona1);
+            a.aggiungiPasseggero(persona2);
 
-            System.out.println(a2.toString());
+            System.out.println(a.toString());
 
-            a2.ordinaPasseggeriCrescente();
-            System.out.println("Passeggeri ordinati in ordine crescente: " + a2.toString());
+            a.ordinaPasseggeriCrescente();
+            System.out.println("Passeggeri ordinati in ordine crescente: " + a.toString());
+
+            a.ordinaPasseggeriDecrescente();
+            System.out.println("Passeggeri ordinati in ordine decrescente: " + a.toString());
         } catch (Exception e) {
             System.out.println("Errore: " + e.getMessage());
         }
